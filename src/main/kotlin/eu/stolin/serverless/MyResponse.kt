a8eccfd2994a368abf45d44d0322baa0ea19439d
Václav Stolín <vaclav.stolin@gmail.comm>
2019-03-18 15:54:18 +0100

    upload do s3 funguje

--- src/main/kotlin/eu/stolin/serverless/MyResponse.kt
+++
@@ -1,3 +0,0 @@
-package eu.stolin.serverless
-
-data class MyResponse(val message: String)
--- src/main/kotlin/eu/stolin/serverless/UploadToS3.kt
+++ src/main/kotlin/eu/stolin/serverless/UploadToS3.kt
@@ -1,18 +1,32 @@
 package eu.stolin.serverless
 
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain
 import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.RequestHandler
 import com.serverless.ApiGatewayResponse
 import org.apache.logging.log4j.LogManager
+import com.amazonaws.services.s3.AmazonS3ClientBuilder
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import java.util.*
+
+const val BUCKET_NAME = "stolin-serverless-text"
+
 
 class UploadToS3 : RequestHandler<Map<String, Any>, ApiGatewayResponse> {
     private val logger = LogManager.getLogger(javaClass)
 
+    private val objectMapper = jacksonObjectMapper()
+
     override fun handleRequest(input:Map<String, Any>, context: Context): ApiGatewayResponse {
         logger.info("input text", input)
 
+        val s3Client = AmazonS3ClientBuilder.standard()
+                .withCredentials(DefaultAWSCredentialsProviderChain.getInstance())
+                .build()
 
+        val body = input["body"] as String
 
+        val response = s3Client.putObject(BUCKET_NAME, UUID.randomUUID().toString(), body)
 
         return ApiGatewayResponse.build {
             statusCode = 201
